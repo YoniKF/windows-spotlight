e8a781c234eaa616e041410d6b7392eb8bebe089
Yoni Katzuv <Yoni Katzuv>
2016-12-20 17:29:41 +0200

    Error handling for read_dir.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ extern crate crypto;
 extern crate image;
 
 use std::env;
+use std::error;
 use std::io::{self, Read};
-use std::fs;
-use std::fs::File;
+use std::fmt;
+use std::fs::{self, File, ReadDir};
 use std::path::Path;
 
 use crypto::digest::Digest;
@@ -19,6 +20,56 @@ static WALLPAPERS_RELATIVE_PATH: &'static str = r#"Pictures\Wallpapers\Windows S
 static JPEG_EXTENSION: &'static str = "jpg";
 
 const FHD: (u32, u32) = (1920, 1080);
+
+#[derive(Debug)]
+enum AssetsReadingError {
+    EnvVar(env::VarError),
+    Io(io::Error),
+}
+
+impl fmt::Display for AssetsReadingError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            AssetsReadingError::EnvVar(ref error) => {
+                write!(formatter, "Environment variable error: {}", error)
+            }
+            AssetsReadingError::Io(ref error) => write!(formatter, "IO error: {}", error),
+        }
+    }
+}
+
+impl error::Error for AssetsReadingError {
+    fn description(&self) -> &str {
+        match *self {
+            AssetsReadingError::EnvVar(ref error) => error.description(),
+            AssetsReadingError::Io(ref error) => error.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            AssetsReadingError::EnvVar(ref error) => Some(error),
+            AssetsReadingError::Io(ref error) => Some(error),
+        }
+    }
+}
+
+impl From<env::VarError> for AssetsReadingError {
+    fn from(error: env::VarError) -> AssetsReadingError {
+        AssetsReadingError::EnvVar(error)
+    }
+}
+
+impl From<io::Error> for AssetsReadingError {
+    fn from(error: io::Error) -> AssetsReadingError {
+        AssetsReadingError::Io(error)
+    }
+}
+
+fn read_assets_directory() -> Result<ReadDir, AssetsReadingError> {
+    Ok(Path::new(&env::var(USER_PROFILE_ENV_VAR)?).join(ASSETS_RELATIVE_PATH)
+        .read_dir()?)
+}
 
 fn is_full_hd_or_better(dimensions: (u32, u32)) -> bool {
     FHD <= dimensions && dimensions.0 * FHD.1 == dimensions.1 * FHD.0
@@ -39,23 +90,28 @@ fn calculate_file_md5_digest(path: &Path) -> io::Result<String> {
 fn main() {
     let user_profile_env_var = env::var(USER_PROFILE_ENV_VAR).unwrap();
     let user_profile = Path::new(&user_profile_env_var);
-    let assets = user_profile.join(ASSETS_RELATIVE_PATH);
     let wallpapers = user_profile.join(WALLPAPERS_RELATIVE_PATH);
 
-    for asset in assets.read_dir().unwrap() {
-        let asset = asset.unwrap();
-        if asset.file_type().unwrap().is_file() {
-            if let Ok(dimensions) = JPEGDecoder::new(File::open(asset.path()).unwrap())
-                .dimensions() {
-                if is_full_hd_or_better(dimensions) {
-                    let path = asset.path();
-                    let mut new_path = wallpapers.join(calculate_file_md5_digest(&path).unwrap());
-                    new_path.set_extension(JPEG_EXTENSION);
-                    if !new_path.exists() {
-                        fs::copy(path, new_path).unwrap();
+    match read_assets_directory() {
+        Ok(assets) => {
+            for asset in assets {
+                let asset = asset.unwrap();
+                if asset.file_type().unwrap().is_file() {
+                    if let Ok(dimensions) = JPEGDecoder::new(File::open(asset.path()).unwrap())
+                        .dimensions() {
+                        if is_full_hd_or_better(dimensions) {
+                            let path = asset.path();
+                            let mut new_path =
+                                wallpapers.join(calculate_file_md5_digest(&path).unwrap());
+                            new_path.set_extension(JPEG_EXTENSION);
+                            if !new_path.exists() {
+                                fs::copy(path, new_path).unwrap();
+                            }
+                        }
                     }
                 }
             }
         }
+        Err(error) => println!("{}", error),
     }
 }
